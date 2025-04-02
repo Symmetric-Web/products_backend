cd8de9e23afde580cc70b7f9ff33df497868a655
Symmetric-Web <symmetricwebsite@gmail.com>
2025-04-02 23:21:26 +0530

    sample

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_data():
         # Get product type from request body
         request_data = request.get_json()
         product_type = request_data.get('product_type')
-        with open(f"json/{product_type}.json","r") as filename:
+        with open(f"json_data/{product_type}.json","r") as filename:
             result = json.load(filename)
         print("hi")
         
