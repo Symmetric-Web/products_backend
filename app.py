a2f5125d75aebf332a624eadc89c8697e866c04b
Symmetric-Web <symmetricwebsite@gmail.com>
2025-04-02 22:59:19 +0530

    new

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import firebase_admin
-from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
 import os
 
@@ -12,17 +10,6 @@ app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://symmetricitservicespvtltd.netlify.app,'https://www.symmetricit.in/','https://symmetricit.in/']}})
 
-# Initialize Firebase Admin based on environment
-if os.getenv('ENVIRONMENT') == 'prod':
-    # Use default credentials in production
-    firebase_admin.initialize_app()
-else:
-    # Use service account file in development
-    cred = credentials.Certificate('../ServiceAccountViewer.json')
-    firebase_admin.initialize_app(cred)
-
-# Get Firestore client
-db = firestore.client()
 
 @app.route('/get_products', methods=['POST'])
 def get_data():
@@ -31,27 +18,16 @@ def get_data():
         # Get product type from request body
         request_data = request.get_json()
         product_type = request_data.get('product_type')
+        with open(f"json/{product_type}.json", "w") as json_file:
+            json.dump(temp_data, json_file,indent=4)
+        with open(f"json/{product_type}.json","r") as filename:
+            result = json.load(filename)
+        print("hi")
         
-        if not product_type:
-            return jsonify({
-                'status': 'error',
-                'message': 'product_type is required in request body'
-            }), 400
-        
-        # Query the collection based on product type
-        products_ref = db.collection(product_type.replace(' ', '-'))
-        docs = products_ref.stream()
-        
-        products = []
-        for doc in docs:
-            products.append({
-                'id': doc.id,
-                **doc.to_dict()
-            })
         
         return jsonify({
             'status': 'success',
-            'data': products
+            'data': result
         }), 200
     
     except Exception as e:
